4424944e1023b65768b69f98c70ca49488e36701
Anakin Raymer <10697005@my.uvu.edu>
2018-09-12 17:18:21 -0600

    Code 2
    Edit: fix for previous code. will need future fixes. Character movement.

--- New Unity Project/Assets/Scripts/CharacterMove.cs
+++ New Unity Project/Assets/Scripts/CharacterMove.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	// Player Movement Variables
 	public int MoveSpeed;
 	public float JumpHeight = 15;
+
+	// Player grounded variables
+	public Transform GroundChecks;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGrounded;
+	private bool Grounded;
 
 	// Use this for initialization
 	void Start () {
@@ -15,5 +22,25 @@ public class CharacterMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	// Update is called once per frame
+	void Update () {
+			Grounded = Physics2D.OverlapCircle(Groundcheck.Position, GroundCheckRadius, WhatIsGround);
+
+		// Update is called once per frame
+		void Update () {
+			if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+				Jump();
+			}
+
+			// This code makes the character move from side to side using the
+			if(Input.GetKey (KeyCode.D)){}
+				GetComponent
+		}
+	}
+	public void Jump(){
+		GetComponent<Rigidbody2D>().Velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+	}
 	}
 }
