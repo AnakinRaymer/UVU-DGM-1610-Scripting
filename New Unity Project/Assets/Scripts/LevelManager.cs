e281714864c3ff77d87a687d37c8e3abfd7ef246
Anakin Raymer <10697005@my.uvu.edu>
2018-10-03 17:15:10 -0600

    Started writing LevelManager

---
+++ New Unity Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject CurrentCheckPoint;
+	private Rigidbody2D Player;
+
+	//Particles
+	public GameObject DeathParticle
+	public GameObject RespawnParticle;
+
+	//Respawn Delay
+	public float RespawnDelay;
+
+	//Point Penalty
+	public int PointPenaltyOnDeath;
+
+	// Store gravity value
+	private float GravityStore;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Rigidbody2D> ();
+	}
+	
+	public void RespawnPlayer(){
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		//Generate Death Particle
+		Instantiate (DeathParticle, player.transform.position, player.transform.rotation);
+		//Hide Player
+		player.enabled = false;
+		player.GetComponent<Renderer> ().enabled = false;
+		// Gravity Reset
+		GravityStore = Player.GetComponent<Rigidbody2D>().GravityScale;
+		Player.GetComponent<Rigidbody2D>().GravityScale = 0f;
+		Player.GetComponent<Rigidbody2D>().Velocity = Vector2.zero;
+		// Point Penalty
+		ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		//Debug Message
+		Debug.Log ("Player Respawn");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Scripts/PopQuiz.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PopQuiz : MonoBehaviour {
+
+	// Quiz
+
+	public static int ColourRed;
+	public static int ColourBlue;
+	public static int ColourPurple;
+
+	Text ColorText;
+
+	// Use this for initialization
+	void Start () {
+		ColorText = GetComponent<Text>();
+		
+		ColorText = ColourRed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ColourRed + ColourBlue)
+		ColourText = 0;
+
+		ColorText.text = " " + ColourPurple;
+	}
+}
