79d6cf7fbca665bee339bebdc89a38f8364e7c4c
Anakin Raymer <10697005@my.uvu.edu>
2018-10-29 17:13:16 -0600

    Added Switches, need to do 5 homework and fix projectile.

---
+++ New Unity Project/Assets/Scripts/Switches.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour {
+
+	public int num;
+	public string Name;
+
+	// Use this for initialization
+	void Start () {
+		if(num == 10){
+			print("Hurray you picked ten!");
+		}
+		
+		switch(num){
+			case 1:
+				print("You picked " + num);
+				break;
+
+			case 3:
+				print("You picked " + num);
+				break;
+
+			case 6:
+				print("You picked " + num);
+				break;
+
+			case 10:
+				print("You picked " + num);
+				break;
+
+			default:
+				print("I dont understand " + num);
+				break;
+				}
+
+		switch(Name){
+			case "Jason":
+				print("Welcome to camp crystal lake " + Name + " - love mother");
+			break;
+
+			case "Michael":
+				print("Welcome to Haddonfield " + Name);
+			break;
+
+			case "Freddie":
+				print("Welcome to Elm Street " + Name);
+			break;
+
+			case "Leatherface":
+				print("The stars and stripes are big at night... Deep in the heart of Texas");
+			break;
+
+			default:
+				print("I dont know " + Name);
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
